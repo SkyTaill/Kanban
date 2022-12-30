a087b53df3259c926f7b6615c23386cebd4cf14c
maxim <vorobeichik2015@yandex.ru>
2022-12-30 23:18:48 +0300

    add menu under profile

--- kanban-front/src/components/Header/index.jsx
+++ kanban-front/src/components/Header/index.jsx
@@ -13,11 +13,18 @@ class Header extends React.Component {
             <header className={css.header}>
                 <div className={css.header_content}>
                     <h1 className={css.text}>Awesome Kanban Board</h1>
-                    <div className={css.header_profil}>
+                    <div className={css.profil}>
+                        <div className={css.header_profil}>
 
-                        <div className={css.svg}></div>
-                        <div className={css.svg2}></div>
+                            <div className={css.svg}></div>
+                            <div className={css.svg2}></div>
+                        </div>
+                        <div className={css.prof_menu}>
+                            <h3 className={css.text_profil}>Profile</h3>
+                            <h3 className={css.logOut}>Log Out</h3>
+                        </div>
                     </div>
+
 
                 </div>
             </header>
